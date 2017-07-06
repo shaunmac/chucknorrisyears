7b18f9ffbb15f4dab3d59e011c8953aee97f67bf
LaurissaCreek <larriethecucumber@gmail.com>
2017-07-06 20:10:41 +1000

    code using button and inputs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+(function(){
+
 var myVideo =  iframe.getElementById('video-bg');
 myVideo.mute();
 
@@ -5,17 +7,31 @@ myVideo.mute();
 //this is where our logic will go
 // var norrisAge = 77;
 // march 10, 1940 birthday calculation value
-var d = new Date();
-var norrisAgeCalculation = d.getFullYear() - 1940;
-// console.log(norrisAgeCalculation);
+/*function ageCalculation() {
+  var d = new Date();
+  var norrisAgeCalculation = d.getFullYear() - 1940;
+  // console.log(norrisAgeCalculation);
 
-var x = 13;
+  var x = document.querySelectorAll(".form-control");
 
 
-if (x > 12) {
-  // console.log("hello");
-  var newAge = 21 / norrisAgeCalculation * x;
-  console.log(Math.round(newAge + 9));
-} else {
-  console.log('sorry you are too young for this calculation');
-}
+  if (x > 12) {
+    // console.log("hello");
+    var newAge = 21 / norrisAgeCalculation * x;
+    console.log(Math.round(newAge + 9));
+  } else {
+    console.log('sorry you are too young for this calculation');
+  }
+};
+console.log("working");
+
+document.getElementById('btn').addEventListener('click', function(){
+//document.body.innerHTML = chosenHouse + ' has been chosen';
+ageCalculation();
+console.log('button was clicked');
+*/
+
+console.log("working");
+//<input type="text" class="form-control" placeholder="My age Is...">
+//<input class="btn" type="submit" value="Submit" />
+}());
